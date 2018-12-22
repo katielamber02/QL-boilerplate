fe81be2291038e5ff1e23d2ee8dd9d7b40eca4b5
katielamber02 <32227217+katielamber02@users.noreply.github.com>
2018-12-22 16:18:19 +0200

    mock books request

--- server/app.js
+++ server/app.js
@@ -1,10 +1,15 @@
-const express=require('express')
-const graphqlHTTP=require('express-graphql')
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const schema = require('./schema/schema');
 
-const app=express()
+const app = express();
 
-app.use('/graphql',graphqlHTTP({
-  
-}))
+// bind express with graphql
+app.use('/graphql', graphqlHTTP({
+    schema,
+    graphiql:true
+}));
 
-app.listen(4000,console.log(`Server is listening on ${4000}`))
+app.listen(4000, () => {
+    console.log('now listening for requests on port 4000');
+});
