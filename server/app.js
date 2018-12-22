7f4b51436a952dfecb80c3406955c292c53ab7e5
katielamber02 <32227217+katielamber02@users.noreply.github.com>
2018-12-22 17:35:38 +0200

    connection to DB

--- server/app.js
+++ server/app.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose=require('mongoose')
+
 
 const app = express();
+
+mongoose.connect('mongodb://katielamber02:begginYou6342@ds141654.mlab.com:41654/db-gql',
+ {useNewUrlParser:true}
+)
+mongoose.connection.once('open',()=>{
+    console.log('connected to database')
+})
+
 
 // bind express with graphql
 app.use('/graphql', graphqlHTTP({
