3fb6026165cacb0d9505e23f20866aa975e26168
katielamber02 <32227217+katielamber02@users.noreply.github.com>
2018-12-28 12:18:05 +0200

    books-competed

--- server/app.js
+++ server/app.js
@@ -2,9 +2,11 @@ const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
 const mongoose=require('mongoose')
-
+const cors=require('cors')
 
 const app = express();
+
+app.use(cors())
 
 mongoose.connect('mongodb://katielamber02:begginYou6342@ds141654.mlab.com:41654/db-gql',
  {useNewUrlParser:true}
--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
 import ApolloClient from "apollo-boost";
 import { ApolloProvider } from "react-apollo";
-import BookList from './BookList'
+import BookList from './components/BookList'
+import AddBook from './components/AddBook'
 
 //apollo client setup
 
@@ -16,22 +16,11 @@ class App extends Component {
   render() {
     return (
       <ApolloProvider client={client}>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+      <div id="main">
+          <h1>Reading List:</h1>
           <BookList />
-        </header>
+          <AddBook />
+          
       </div>
       </ApolloProvider>
     );
